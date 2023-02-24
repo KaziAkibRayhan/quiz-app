f49f10d814351480ca20facf583e24da23126b2d
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-02-24 22:35:08 +0600

    Quiz component adde

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Quiz = () => {
-    return (
-        <div>
-            Quiz
-        </div>
-    );
+    const onNext = () =>{
+        console.log('On Next');
+    }
+    const onPrev = () =>{
+        console.log('On Prev');
+    }
+
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+      {/* Display Questions */}
+
+      <div className="grid">
+        <button className="btn prev" onClick={onPrev}>Previous</button>
+        <button className="btn next" onClick={onNext}>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
